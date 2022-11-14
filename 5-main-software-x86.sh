67ee7d0d08c6e0fadcb1532d6dc042e6d7eeeedb
starfire555 <76488394+starfire555@users.noreply.github.com>
2022-11-14 15:37:23 +0000

    Create 5-main-software-x86.sh

---
+++ 5-main-software-x86.sh
@@ -0,0 +1,6 @@
+sudo pacman -S --noconfirm neofetch btop plocate arch-install-scripts firefox telegram-desktop community/code obsidian nomacs vlc audacity libreoffice gimp flameshot remmina youtube-dl pavucontrol qbittorrent
+
+echo ""
+echo "######################################################"
+echo "### Main software install completed successfully.  ###"
+echo "######################################################"
