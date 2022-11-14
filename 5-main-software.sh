4bce6f7f68716939e3544decd8a450935008c693
starfire555 <76488394+starfire555@users.noreply.github.com>
2022-11-14 09:14:18 +0000

    Update 5-main-software.sh

--- 5-main-software.sh
+++ 5-main-software.sh
@@ -1,5 +1,7 @@
-sudo pacman -S --noconfirm neofetch btop plocate arch-install-scripts firefox telegram-desktop community/code obsidian nomacs vlc audacity simplescreenrecorder libreoffice gimp flameshot remmina youtube-dl icecast pavucontrol qbittorrent icecast
+sudo pacman -S --noconfirm neofetch btop plocate arch-install-scripts firefox telegram-desktop community/code obsidian nomacs vlc audacity simplescreenrecorder libreoffice gimp flameshot remmina youtube-dl pavucontrol qbittorrent icecast
+read -p "Press enter to continue"
 sudo pacman -S virt-manager qemu qemu-arch-extra ovmf vde2 ebtables dnsmasq bridge-utils openbsd-netcat
+read -p "Press enter to continue"
 sudo pacman -S --noconfirm cockpit
 sudo pacman -S --noconfirm --asdeps freerdp spice-gtk cockpit-pcp cockpit-storaged cockpit-packagekit cockpit-podman cockpit-machines
 sudo systemctl enable icecast
