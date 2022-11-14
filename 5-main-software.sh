4576322eb76bbecc09c92e5bd0b0396409ca492d
starfire555 <76488394+starfire555@users.noreply.github.com>
2022-11-14 08:26:19 +0000

    Update 5-main-software.sh

--- 5-main-software.sh
+++ 5-main-software.sh
@@ -1,4 +1,4 @@
-sudo pacman -S --noconfirm neofetch btop plocate arch-install-scripts firefox telegram-desktop community/code obsidian nomacs vlc audacity simplescreenrecorder libreoffice gimp flameshot remmina youtube-dl icecast pavucontrol qbittorrent cockpit virt-install qemu-base
+sudo pacman -S --noconfirm neofetch btop plocate arch-install-scripts firefox telegram-desktop community/code obsidian nomacs vlc audacity simplescreenrecorder libreoffice gimp flameshot remmina youtube-dl icecast pavucontrol qbittorrent cockpit virt-manager qemu qemu-arch-extra ovmf vde2 ebtables dnsmasq bridge-utils openbsd-netcat
 sudo pacman -S --noconfirm --asdeps freerdp spice-gtk cockpit-pcp cockpit-storaged cockpit-packagekit cockpit-podman cockpit-machines
 sudo systemctl enable cockpit.socket
 sudo systemctl enable libvirtd
