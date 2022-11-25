7f12022ceb7610a41568dac8c4dc1a3d0e17da59
starfire555 <76488394+starfire555@users.noreply.github.com>
2022-11-25 20:56:08 +0000

    Create 4-xfce-install.sh

---
+++ 4-xfce-install.sh
@@ -0,0 +1,16 @@
+### xfce Install ###
+echo ">>> Installing xfce"
+sudo pacman -S xorg lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings xfce4 xfce4-goodies arc-gtk-theme arc-icon-theme papirus-icon-theme
+sudo systemctl set-default graphical
+sudo systemctl enable lightdm
+
+echo '#!/bin/bash' > /home/x/.xinitrc
+echo 'startxfce4' >> /home/x/.xinitrc
+chmod +x /home/x/.xinitrc
+echo "..........................................................."
+
+echo ""
+echo "############################################"
+echo "### xfce install completed successfully. ###"
+echo "############################################"
+echo ""
